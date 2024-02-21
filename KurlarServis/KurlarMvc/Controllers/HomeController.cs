977557a1e35391b396f6143be6b3aa9cd33b5243
eren-22 <erenozz@yahoo.com>
2024-02-21 16:51:49 +0300

    An MVC project that displays exchange rates using WCF.
    An MVC project that displays the last 15 days of exchange rates using WCF on the service side.

---
+++ KurlarServis/KurlarMvc/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KurlarMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            using(KurServis.KurServisClient servisClient = new KurServis.KurServisClient())  //KurServis sınıfından nesne oluşturulur.
+            {
+                if (TempData["Kurlar"] != null)
+                {
+                    ViewBag.Kurlar = (List<double>)TempData["Kurlar"];
+                    //ViewBag verileri controllerdan view'a iletmek için kullanılır.
+                }
+                else
+                {
+                    ViewBag.Kurlar = new List<double>();
+                }
+
+                var model = servisClient.paraBirimleriGetir().ToList();
+
+                return View(model);
+            }
+        }
+
+        public ActionResult KurGetir(string paraBirimi)
+        {
+            using (KurServis.KurServisClient servisClient = new KurServis.KurServisClient())
+            {
+                var model = servisClient.kurlariGetir(paraBirimi).ToList();
+                TempData["Kurlar"] = model;
+                return RedirectToAction("Index");
+            }
+                  
+        }
+    }
+}
---
+++ KurlarServis/KurlarServis/IKurServis.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace KurlarServis
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IKurServis
+    {
+
+        [OperationContract] //Servis olarak verileceğini gösteriyor.
+        List<string> paraBirimleriGetir();
+
+        [OperationContract]
+        List<double> kurlariGetir(string kurTipi);
+
+        // TODO: Add your service operations here
+    }
+
+
+   
+}
